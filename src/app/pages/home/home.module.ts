69b1749df5efa1873dbdef84d106a709b24601f5
aliclic <alicvictor11@gmail.com>
2023-10-21 14:24:05 -0300

    criacao de uma simples pagina home, pequena alteração no footer

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,18 +1,26 @@
+import { faChartBar, faWallet, faLineChart, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 
-
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
   declarations: [
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FontAwesomeModule,
+    RouterLink
   ],
   exports: [
     HomeComponent
   ]
 })
-export class HomeModule { }
+export class HomeModule { 
+    constructor(library: FaIconLibrary) {
+      library.addIcons(faChartBar, faWallet, faLineChart, faPhone, faEnvelope);
+    }
+}
