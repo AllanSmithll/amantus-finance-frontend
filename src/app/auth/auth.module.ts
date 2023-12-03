bfe78ef403f1efb42b409525eb6578e51e6d84b0
aliclic <alicvictor11@gmail.com>
2023-12-03 16:56:13 -0300

    atualizacao do design login e register

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,12 @@
+import { MatCardModule } from '@angular/material/card';
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RouterModule } from '@angular/router';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEnvelope, faLock, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [
@@ -16,11 +19,18 @@ import {CommonModule} from "@angular/common";
             {path: 'login', component: LoginComponent},
             {path: 'register', component: RegisterComponent}
         ]),
-        FormsModule
+        FormsModule,
+        MatCardModule,
+        FontAwesomeModule,
+        ReactiveFormsModule
   ],
   exports: [
     LoginComponent,
     RegisterComponent
   ]
 })
-export class AuthModule { }
+export class AuthModule { 
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faEnvelope, faLock, faUser);
+  }
+}
