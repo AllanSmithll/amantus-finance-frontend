0fe764aff285c9bafe0d45b27ecdc06741f2c396
allansmithll <allanammancio2004@gmail.com>
2023-12-19 17:27:52 -0300

    Progredindo no serviço de usuário
    Implementando um serviço e CRUD para o usuário.

--- src/app/pages/configs/configs.component.ts
+++ src/app/pages/configs/configs.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/shared/models/user.model';
 import { AuthenticationService } from 'src/app/shared/services/authentication.service';
-import { UserService } from 'src/app/shared/services/user.service';
+import {UserService} from "../../shared/services/user.service";
+import {MenssageService} from "../../shared/services/menssage.service";
 
 @Component({
   selector: 'app-configs',
@@ -11,19 +11,25 @@ import { UserService } from 'src/app/shared/services/user.service';
 export class ConfigsComponent implements OnInit{
   user: any = {};
 
-  constructor(private userService: UserService) {}
+  constructor(private authService: AuthenticationService, private userService: UserService,
+              private mensageService: MenssageService) {}
 
   ngOnInit(): void {
-    this.userService.getCurrentUser().subscribe(
+    this.authService.getCurrentUser().subscribe(
       (user) => {
         this.user = user;
-      }
-    );
+      });
   }
 
   saveChanges(): void {
-    // logica  
-    console.log('informações atualizadas!');
+    this.userService.update(this.user).subscribe(
+      (updatedUser) => {
+        this.user = updatedUser;
+        this.mensageService.showSuccess('Dados do usuário atualizados com sucesso!');
+      },
+      (error) => {
+        this.mensageService.showError('Erro ao atualizar os dados do usuário!');
+      }
+    );
   }
-  
 }
